7820283ad15724525f44f2280a986170367f124d
Adebimpe12 <156775119+Adebimpe12@users.noreply.github.com>
2024-04-09 12:56:18 +0100

    Add files via upload

---
+++ Projects/django.py
@@ -0,0 +1,35 @@
+deployment- deploy your model on web application, web backend,
+software platform deployment eg web services, desktop dpeliyment eg on laptop and hardware platform deployment eg robot, flatscreen, self driving car
+web crwaling, web scraping, data mininng 
+full stack developer after level 3, be employed
+app- frontend, backend- database, server
+server recieves data and processes the data, collects information from frontedn and sends it frontend ,database- stoes information permanently
+django developer and ai developer are backend developer
+expected to know server and understand databse as backend developer
+frontend javascript, angular, react
+backened- server- python, php, node js, laravel, .het,; database - oracle,mysql, sqlite, ms server, mongo (sql ii)
+
+python-game, app, desktop app, execeutable file for install, web asppllication, iot, robotic, mobile app, ios/android app
+web backend - django, flask, bottle, web2py, streamlit
+python web frameworks - cherrypy, grok, turbogears, tornado,bluebream
+api- 
+django - complex code quicky
+define patterns
+authentication 
+oop
+automatic admin
+url system
+virtual environment- other environment, create a vitual environment for each individual project
+face recognition- need libraries, tensorflow, keras, matplotlib, open cv, numpy ai project
+object detector- need tensorflow, opencv, numpy, keras, pandas ai project
+
+anaconda prompt
+inside location django file
+pip install virtualenv
+virtualenv musicAppEnv
+lib
+site-packages
+activate virtualenv instead of BaseExceptioncd musicappenv
+scripts\activate
+once activated, go out of environment cd..
+to deactivate environment- deactivate
